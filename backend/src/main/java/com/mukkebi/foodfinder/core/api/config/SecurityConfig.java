266a26685af4f2d382ed9167857c0a6984498001
rlatjddms <kseo_o@naver.com>
2025-12-27 21:11:53 +0900

    refactor(user): OAuth2 로그인 관련 인가 설정 정리

--- backend/src/main/java/com/mukkebi/foodfinder/core/api/config/SecurityConfig.java
+++ backend/src/main/java/com/mukkebi/foodfinder/core/api/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.mukkebi.foodfinder.core.api.config;
 
-import com.mukkebi.foodfinder.core.domain.CustomOAuth2UserService;
+import com.mukkebi.foodfinder.core.support.security.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -14,20 +17,26 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login/**").permitAll()
+                        .requestMatchers(
+                                "/oauth2/**",
+                                "/api/auth/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth -> oauth
-                        .userInfoEndpoint(user ->
-                                user.userService(customOAuth2UserService)
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo ->
+                                userInfo.userService(customOAuth2UserService)
                         )
                 );
 
         return http.build();
     }
 }
-
