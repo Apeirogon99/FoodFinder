a161ee5174000e28f05fbe6e24ef198e7e0de17c
rlatjddms <kseo_o@naver.com>
2025-12-27 21:14:46 +0900

    feat(user): 회원가입 API 추가

--- backend/src/main/java/com/mukkebi/foodfinder/core/api/controller/v1/UserController.java
+++ backend/src/main/java/com/mukkebi/foodfinder/core/api/controller/v1/UserController.java
@@ -4,15 +4,12 @@ import com.mukkebi.foodfinder.core.api.controller.v1.request.UpdateProfileReques
 import com.mukkebi.foodfinder.core.api.controller.v1.response.UserProfileResponse;
 import com.mukkebi.foodfinder.core.domain.UserService;
 import com.mukkebi.foodfinder.core.support.response.ApiResult;
+import com.mukkebi.foodfinder.core.support.security.OAuthUserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,29 +17,14 @@ public class UserController {
 
     private final UserService userService;
 
-    // 로그인 성공 메세지 API
-    @GetMapping("/api/users/login-success")
-    public ApiResult<String> loginSuccess() {
-        return ApiResult.success("GitHub 로그인 성공!");
-    }
-
-    // 로그인한 사용자의 GitHub 정보 조회 API
-    @GetMapping("/github-profile")
-    public ApiResult<Map<String, Object>> githubProfile(
-            @AuthenticationPrincipal OAuth2User oauth2User
-    ) {
-        return ApiResult.success(oauth2User.getAttributes());
-    }
-
-    // 회원가입 시 프로필 입력 API
-    @PostMapping("/profile")
-    public ApiResult<UserProfileResponse> updateProfile(
-            @AuthenticationPrincipal OAuth2User oauth2User,
+    // 회원가입
+    @PostMapping("/api/users/signup")
+    public ApiResult<UserProfileResponse> signUp(
+            @AuthenticationPrincipal OAuthUserPrincipal principal,
             @RequestBody UpdateProfileRequest request
     ) {
-        String githubId = oauth2User.getAttribute("login");
-        UserProfileResponse response = userService.updateMyProfile(githubId, request);
-
-        return ApiResult.success(response);
+        return ApiResult.success(
+                userService.signUp(principal.getUserId(), request)
+        );
     }
 }
